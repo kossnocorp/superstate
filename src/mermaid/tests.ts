a0194735d9e02c3370c20a992b43e7dcd4dfa269
Sasha Koss <koss@nocorp.me>
2024-04-04 12:21:09 +0800

    Kill old code and tests

--- src/mermaid/tests.ts
+++ src/mermaid/tests.ts
@@ -1,32 +1,29 @@
 import { describe, expect, it } from "vitest";
-import { QMermaid } from "./index.js";
-import { Q } from "../index.js";
+// import { QMermaid } from "./index.js";
+// import { Q } from "../index.js";
 
-describe("QMermaid", () => {
-  type SoundState = "playing" | "paused" | "stopped";
-
-  type SoundAction = "play" | "pause" | "stop";
-
-  class Sound extends Q.Machine<SoundState, SoundAction> {
-    constructor() {
-      super("sound", [
-        "[*] --> stopped",
-        "playing --> paused: pause",
-        "playing --> stopped: stop",
-        "paused --> playing: play",
-        "paused --> stopped: stop",
-        "stopped --> playing: play",
-      ]);
-    }
-  }
-
-  describe("generate", () => {
-    describe("Machine", () => {
-      it("generates a mermaid diagram from a machine", () => {
-        const sound = new Sound();
-        const mermaid = QMermaid.generate(sound);
-        expect(mermaid).toMatchSnapshot();
-      });
-    });
-  });
+describe.todo("Mermaid", () => {
+  //   type SoundState = "playing" | "paused" | "stopped";
+  //   type SoundAction = "play" | "pause" | "stop";
+  //   class Sound extends Q.Machine<SoundState, SoundAction> {
+  //     constructor() {
+  //       super("sound", [
+  //         "[*] --> stopped",
+  //         "playing --> paused: pause",
+  //         "playing --> stopped: stop",
+  //         "paused --> playing: play",
+  //         "paused --> stopped: stop",
+  //         "stopped --> playing: play",
+  //       ]);
+  //     }
+  //   }
+  //   describe("generate", () => {
+  //     describe("Machine", () => {
+  //       it("generates a mermaid diagram from a machine", () => {
+  //         const sound = new Sound();
+  //         const mermaid = QMermaid.generate(sound);
+  //         expect(mermaid).toMatchSnapshot();
+  //       });
+  //     });
+  //   });
 });
